8d899b2170e433a812e3e7f7e8626d6342e1916e
TharinduDakshina <tharindudakshina527@gmail.com>
2025-11-10 12:25:37 +0530

    add the middleware for the application and manage the routs

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import {NextRequest, NextResponse} from "next/server";
+
+export function middleware(request: NextRequest){
+    const token = request.cookies.get("token")?.value;
+    const protectedRouts = ["/components/dashboard", "/components/carCustomizer"];
+
+    if (protectedRouts.includes(request.nextUrl.pathname)) {
+        if (!token) {
+            return  NextResponse.redirect(new URL("/components/signIn", request.url));
+        }
+    }
+    return  NextResponse.next();
+}
